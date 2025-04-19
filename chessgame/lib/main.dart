5c4cb4ed86719ce7e1c6f84a217b65772d712a14
dinh nam <nguyendinhnama1k51dta@gmail.com>
2025-04-19 10:43:39 +0700

    home_sreen

--- chessgame/lib/main.dart
+++ chessgame/lib/main.dart
@@ -1,157 +1,18 @@
 import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChessGameApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class ChessGameApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter Chess Game',
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
+      home: HomeScreen(),
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: Colors.grey[100],
-      ),
-      home: TodoScreen(),
-    );
-  }
-}
-
-class TodoScreen extends StatefulWidget {
-  @override
-  _TodoScreenState createState() => _TodoScreenState();
-}
-
-class _TodoScreenState extends State<TodoScreen> {
-  List<Map<String, dynamic>> _tasks = [
-    {'title': 'Học Dart', 'completed': false},
-    {'title': 'Học Flutt  er', 'completed': false},
-    {'title': 'Học Firebase', 'completed': false}
-  ];
-  final TextEditingController _taskController = TextEditingController();
-
-  void _addTask(String task) {
-    setState(() {
-      _tasks.add({'title': task, 'completed': false});
-    });
-  }
-
-  void _toggleTask(int index) {
-    setState(() {
-      _tasks[index]['completed'] = !_tasks[index]['completed'];
-    });
-  }
-
-  void _removeTask(int index) {
-    setState(() {
-      _tasks.removeAt(index);
-    });
-  }
-
-  void _showAddTaskDialog() {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text(
-            'Thêm công việc mới',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.blue[800],
-            ),
-          ),
-          content: TextField(
-            controller: _taskController,
-            decoration: InputDecoration(
-              hintText: 'Nhập tên công việc',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-            ),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(),
-              child: Text(
-                'Hủy',
-                style: TextStyle(color: Colors.red),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                if (_taskController.text.isNotEmpty) {
-                  _addTask(_taskController.text);
-                  _taskController.clear();
-                  Navigator.of(context).pop();
-                }
-              },
-              child: Text('Thêm'),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue[800],
-              ),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'To-do App',
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        backgroundColor: Colors.blue[800],
-        elevation: 2,
-      ),
-      body: ListView.builder(
-        padding: EdgeInsets.symmetric(vertical: 8),
-        itemCount: _tasks.length,
-        itemBuilder: (context, index) {
-          return Card(
-            margin: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-            elevation: 2,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(8),
-            ),
-            child: ListTile(
-              leading: Checkbox(
-                value: _tasks[index]['completed'],
-                onChanged: (_) => _toggleTask(index),
-                activeColor: Colors.blue[800],
-              ),
-              title: Text(
-                _tasks[index]['title'],
-                style: TextStyle(
-                  fontSize: 16,
-                  decoration: _tasks[index]['completed']
-                      ? TextDecoration.lineThrough
-                      : TextDecoration.none,
-                  color: _tasks[index]['completed'] ? Colors.grey : Colors.black87,
-                ),
-              ),
-              trailing: IconButton(
-                icon: Icon(Icons.delete, color: Colors.red[800]),
-                onPressed: () => _removeTask(index),
-              ),
-            ),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _showAddTaskDialog,
-        child: Icon(Icons.add, color: Colors.white),
-        backgroundColor: Colors.blue[800],
-      ),
     );
   }
 }
---
+++ chessgame/lib/screens/home_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Chess Game')),
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
