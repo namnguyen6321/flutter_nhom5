68bfa67b82d7d88e52558e59c83f0bedcf8dbc86
dinh nam <nguyendinhnama1k51dta@gmail.com>
2025-04-19 09:57:49 +0700

    khoi tao du an

--- chessgame/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ chessgame/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\dev\tools\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\dev\flutter_nhom5\chessgame"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Admin\Desktop\my_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
