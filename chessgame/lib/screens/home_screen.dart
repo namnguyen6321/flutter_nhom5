c53a5b790964f5a69c79863d62afc3f889b82291
dinh nam <nguyendinhnama1k51dta@gmail.com>
2025-04-26 11:24:44 +0700

    update homepage

--- chessgame/lib/screens/home_screen.dart
+++ chessgame/lib/screens/home_screen.dart
@@ -6,37 +6,124 @@ import 'package:my_app/screens/settings_creen.dart';
 import 'about_screen.dart';
 
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Chess Game')),
-      body: Padding(
-        padding: const EdgeInsets.all(24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () => Navigator.push(context,
-                  MaterialPageRoute(builder: (_) => PlayAIScreen())),
-              child: Text('Play vs AI'),),
+      appBar: AppBar(title: const Text('Chess Game')),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+    GestureDetector(
+      onTap: (){
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const PlayAIScreen()),
+        );
+      },
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            color: Colors.deepPurple,
+            width: 200,
+            height: 200,  
+          ),
+          const SizedBox(width: 10),
+          Padding(
+            padding:  EdgeInsets.only(top: 10),
+            child: Text('Play with AI', style: TextStyle(fontSize: 20,color: Colors.black)),
+          ),
+         
+      
+        ],
+        
+      ),
+    ),
+    Container(
+      height: 10,
+    ),
+    GestureDetector(
+      onTap: (){
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const PlayFriendScreen()),
+        );
+      },
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            color: Colors.deepPurple[400],
+             width: 200,
+            height: 200,
+          ),
+           SizedBox(width: 10),
+          Padding(
+            padding:  EdgeInsets.only(top: 10),
+            child: Text('Play with Friend', style: TextStyle(fontSize: 20,color: Colors.black)),
+          ),
+        ],
+      ),
+    ),
+    Container(
+      height: 10,
+    ),
+    Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          color: Colors.deepPurple[300], 
+          width: 200,
+          height: 200,
+        ),
+        SizedBox(width: 10),
+        Padding(
+          padding:  EdgeInsets.only(top: 10),
+          child: Text('Intructions', style: TextStyle(fontSize: 20,color: Colors.black)),
+        ),
+      ],
+    ),
+  ],
+),
 
-              SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () => Navigator.push(context,
-                    MaterialPageRoute(builder: (_) => PlayFriendScreen())),
-                child: Text('Play with Friend'),
-              ),
-              SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () => Navigator.push(context,
-                  MaterialPageRoute(builder: (_) => SettingsScreen())),
-              child: Text('Settings'),
+      drawer: Drawer(
+        backgroundColor: Colors.deepPurple[100],
+        child: Column(
+          children: [
+            DrawerHeader(
+              child: Icon(Icons.favorite, size: 100, color: Colors.deepPurple),
             ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () => Navigator.push(context,
-                  MaterialPageRoute(builder: (_) => AboutScreen())),
-              child: Text('About'),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home Page'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HomeScreen()),
+                );
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SettingsScreen()),
+                );
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('profile'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const AboutScreen()),
+                );
+              },
             ),
           ],
         ),
--- chessgame/lib/screens/play_ai_screen.dart
+++ chessgame/lib/screens/play_ai_screen.dart
@@ -1,11 +1,14 @@
+
 import 'package:flutter/material.dart';
 
 class PlayAIScreen extends StatelessWidget {
+  const PlayAIScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Play vs AI')),
-      body: Center(
+      appBar: AppBar(title: const Text('Play vs AI')),
+      body: const Center(
         child: Text('Chơi với AI - bàn cờ sẽ hiển thị ở đây'),
       ),
     );
